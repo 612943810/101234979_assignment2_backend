f8ea8ebedb788a40f6fd840251559237f5d4f1bd
Raiyan Rofiquzzaman <raiyanrofiquzzaman@hotmail.com>
2021-12-05 00:00:15 -0500

    This part makes my backend compatible with web services.

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 let express=require('express');
-let mongoose=require('mongoose')
+let mongoose=require('mongoose');
+let cors=require('cors')
 let bodyParser=require('body-parser')
 const employeeModel=require('./model/model')
 const employeeRouter=require('./controller/employeeRoutes.js')
 let app=express();
 app.use(express.json());
+app.use(cors({origin:true,credentials:true}))
 mongoose.connect("mongodb+srv://root:mongodb@comp3123.wcgkp.mongodb.net/101234979_assignment2?retryWrites=true&w=majority",{
     useNewURLParser:true,
     useUnifiedTopology:true
 });
 app.use(employeeRouter);
-    
+
 app.listen(8080,()=>{
 
 });
